fb055abc35183777172215db6ac7ff84b883bc60
kyo1337 <kyuoko13@gmail.com>
2020-05-04 14:23:43 +0700

    Create bing.php

---
+++ bing.php
@@ -0,0 +1,59 @@
+<?php
+
+echo ("######################################################\n");
+echo ("#              Auto Dorker Bing Single               #\n");
+echo ("######################################################\n");
+echo ("# Create By : Kyuoko                                 #\n");
+echo ("# Code : PHP                                         #\n");
+echo ("# Thx To UKL-TEAM, GFS-TEAM, AND YOU                 #\n");
+echo ("######################################################\n");
+echo ("# usage : php bing.php `inurl:/buy.php` > output.txt #\n");
+echo ("######################################################\n");
+
+set_time_limit(0);
+error_reporting(0);
+function getsource($url) {
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $content = curl_exec($curl);
+    curl_close($curl);
+    return $content;
+}
+
+$do = urlencode($argv[1]);
+if (isset($argv[1])) {
+    $npage    = 1;
+    $npages   = 30000;
+    $allLinks = [];
+    $lll      = [];
+    while ($npage <= $npages) {
+        $x = getsource("http://www.bing.com/search?q=" . $do . "&first=" . $npage);
+        if ($x) {
+            preg_match_all('#<h2><a href="(.*?)" h="ID#', $x, $findlink);
+            foreach ($findlink[1] as $fl) {
+                array_push($allLinks, $fl);
+            }
+
+            $npage = $npage + 10;
+            if (preg_match("(first=" . $npage . "&amp)siU", $x, $linksuiv) == 0) {
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+    $URLs = [];
+    foreach ($allLinks as $url) {
+        $exp    = explode("/", $url);
+        $URLs[] = $exp[2];
+    }
+    $array = array_filter($URLs);
+    $array = array_unique($array);
+    $sss   = count(array_unique($array));
+    //echo "ToTaL SiTe : $sss\n";
+    //echo "--------------------------------------\n";
+    foreach ($array as $domain) {
+        echo "http://$domain/\n";
+    }
+}
+?>
